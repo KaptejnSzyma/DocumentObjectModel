254af8d49dffbf3dd0faf9a66502d3a980f1218a
Szymek <szymon.rzadca@gmail.com>
2018-08-04 10:09:11 +0200

    basic mouse over event

--- Part3.js
+++ Part3.js
@@ -2,4 +2,7 @@ var headOne = document.querySelector('#one')
 var headTwo = document.querySelector('#two')
 var headThree = document.querySelector('#three')
 
-console.log("Connected!");
+headOne.addEventListener('mouseover', function(){
+  headOne.textContent = " Mouse Currently Over";
+  headOne.style.color = "red";
+})
